938f90d5d9b3663bf98d73bfb3d6f07ca4ba1d41
Karel Koudelka <k.koudelka01@gmail.com>
2021-11-18 01:27:10 +0000

    fix: endpoint

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -4,6 +4,6 @@ const endpoint = "https://unusann.us";
 const isDev = process.env.NODE_ENV === "development";
 const localApi = isDev
   ? "http://localhost:3000/api"
-  : "https://uaarchive.withmarkiplier.com/api";
+  : "https://beta.unusann.us/api";
 
 export ***REMOVED*** endpoint, localApi ***REMOVED***;
