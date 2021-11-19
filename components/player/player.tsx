3760032b886a203769434dc8db1a70972cfef7c0
ScribbleNerd <contact@scribblenerd.com>
2021-11-19 17:38:58 -0600

    Actually added i18n to plyr

--- components/player/player.tsx
+++ components/player/player.tsx
@@ -8,6 +8,7 @@ import SkipNextIcon from "@mui/icons-material/SkipNext";
 import Link from "next/link";
 import ***REMOVED*** useRouter ***REMOVED*** from "next/router";
 import ***REMOVED*** NextEpisodeButton ***REMOVED*** from "../episodes-controls";
+import ***REMOVED*** useTranslation ***REMOVED*** from 'react-i18next'
 
 interface IProps ***REMOVED***
   video: IVideo;
@@ -19,6 +20,7 @@ const Player: React.FC<IProps> = (***REMOVED*** video, watchCode ***REMOVED***) 
   const [plyr, setPlyr] = useState<Plyr>(null);
   const [customControlsContainer, setCustomControlsContainer] = useState(null);
   const [currentTime, setCurrentTime] = useState(0);
+  const ***REMOVED*** t, i18n ***REMOVED*** = useTranslation()
 
   const router = useRouter();
 
@@ -72,6 +74,52 @@ const Player: React.FC<IProps> = (***REMOVED*** video, watchCode ***REMOVED***) 
         "fullscreen",
       ],
       ratio: "16:9",
+      i18n: ***REMOVED***
+        //theres probably a much more efficient way to do this but I don't have much time rn lmao
+        restart: t('plyr:restart'),
+        rewind: t('plyr:rewind'),
+        play: t('plyr:play'),
+        pause: t('plyr:pause'),
+        fastForward: t('plyr:fastForward'),
+        seek: t('plyr:seek'),
+        seekLabel: t('plyr:seekLabel'),
+        played: t('plyr:played'),
+        buffered: t('plyr:buffered'),
+        currentTime: t('plyr:currentTime'),
+        duration: t('plyr:duration'),
+        volume: t('plyr:volume'),
+        mute: t('plyr:mute'),
+        unmute: t('plyr:unmute'),
+        enableCaptions: t('plyr:enableCaptions'),
+        disableCaptions: t('plyr:disableCaptions'),
+        download: t('plyr:download'),
+        enterFullscreen: t('plyr:enterFullscreen'),
+        exitFullscreen: t('plyr:exitFullscreen'),
+        frameTitle: t('plyr:frameTitle'),
+        captions: t('plyr:captions'),
+        settings: t('plyr:settings'),
+        pip: t('plyr:pip'),
+        menuBack: t('plyr:menuBack'),
+        speed: t('plyr:speed'),
+        normal: t('plyr:normal'),
+        quality: t('plyr:quality'),
+        loop: t('plyr:loop'),
+        start: t('plyr:start'),
+        end: t('plyr:end'),
+        all: t('plyr:all'),
+        reset: t('plyr:reset'),
+        disabled: t('plyr:disabled'),
+        enabled: t('plyr:enabled'),
+        advertisement: t('plyr:advertisement'),
+        qualityBadge: ***REMOVED***
+          2160: '4K',
+          1440: 'HD',
+          1080: 'HD',
+          720: 'HD',
+          576: 'SD',
+          480: 'SD',
+      ***REMOVED***
+  ***REMOVED***
 ***REMOVED***);
     player.source = ***REMOVED***
       type: "video",
