d19267a1b311ffd6fb01efb8913c55c0be27bad5
ScribbleNerd <contact@scribblenerd.com>
2021-11-20 00:36:35 -0600

    Attempt to fix subtitles on production build

--- pages/api/subtitles.ts
+++ pages/api/subtitles.ts
@@ -23,6 +23,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const text = await subtitles.text();
 
     res.setHeader("Content-Type", "text/vtt");
+    res.setHeader('Access-Control-Allow-Origin', 'unusann.us')
     res.status(200).send(text);
   } catch (error) {
     console.log(error);
