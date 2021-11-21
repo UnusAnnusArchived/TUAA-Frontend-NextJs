24018bbf75f6a22061ad58412dfc53b4819577c5
ScribbleNerd <contact@scribblenerd.com>
2021-11-20 20:08:17 -0600

    Make home page's title as home

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Page: React.FC<IProps> = (***REMOVED*** seasons ***REMOVED***) => ***REMOV
 
   return (
     <Layout>
-      <MetaHead />
+      <MetaHead baseTitle="Home" />
       <div>
         <Tabs value=***REMOVED***currentTab***REMOVED*** onChange=***REMOVED***handleChange***REMOVED*** centered>
           <Tab label=***REMOVED***t("seasons:season1")***REMOVED*** value=***REMOVED***1***REMOVED*** />
