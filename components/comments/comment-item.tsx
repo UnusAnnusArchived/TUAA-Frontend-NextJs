54dd228104993ac856bbe66ec3d13944e7a3cb4f
ScribbleNerd <contact@scribblenerd.com>
2022-02-16 00:11:52 -0600

    add a fix

--- components/comments/comment-item.tsx
+++ components/comments/comment-item.tsx
@@ -20,7 +20,7 @@ const CommentItem: React.FC<IProps> = (***REMOVED*** comment ***REMOVED***) => *
         <div className="col-2 col-md-1 mb-md-0 my-1 d-flex flex-column align-items-center justify-content-center">
           <Avatar
             alt=***REMOVED***comment.user.username***REMOVED***
-            src=***REMOVED***`$***REMOVED***endpoint***REMOVED***$***REMOVED***comment.user.pfp.filename***REMOVED***`***REMOVED***
+            src=***REMOVED***`$***REMOVED***endpoint***REMOVED***$***REMOVED***comment.user.pfp.filename.startsWith("/") ? "" : "/"***REMOVED***$***REMOVED***comment.user.pfp.filename***REMOVED***`***REMOVED***
             sx=***REMOVED******REMOVED*** width: 56, height: 56 ***REMOVED******REMOVED***
           />
         </div>
@@ -31,14 +31,10 @@ const CommentItem: React.FC<IProps> = (***REMOVED*** comment ***REMOVED***) => *
             </Typography>
             &nbsp;
             <Typography variant="body2" sx=***REMOVED******REMOVED*** color: "#a3a3a3" ***REMOVED******REMOVED***>
-              ***REMOVED***moment(new Date(comment.stats.published))
-                .locale(i18n.language)
-                .fromNow()***REMOVED***
+              ***REMOVED***moment(new Date(comment.stats.published)).locale(i18n.language).fromNow()***REMOVED***
             </Typography>
           </div>
-          <Typography
-            dangerouslySetInnerHTML=***REMOVED******REMOVED*** __html: comment.comment.html ***REMOVED******REMOVED***
-          />
+          <Typography dangerouslySetInnerHTML=***REMOVED******REMOVED*** __html: comment.comment.html ***REMOVED******REMOVED*** />
         </div>
       </div>
       <Divider sx=***REMOVED******REMOVED*** backgroundColor: "#fff" ***REMOVED******REMOVED*** />
