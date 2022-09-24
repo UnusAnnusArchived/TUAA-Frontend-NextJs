e8a71297a6c0473a07861ee5f501ac329e71c672
ScribbleNerd <contact@scribblenerd.com>
2022-09-24 00:43:17 -0500

    Add new Patreon Donors

--- pages/patreon-donors.tsx
+++ pages/patreon-donors.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { Divider, Typography } from "@mui/material";
 
 // Import Profile Pictures
+import Alybeanz from "../patreon-donor-pfps/Alybeanz.webp";
 import CaitieP from "../patreon-donor-pfps/Caitie P.webp";
 import CHGJake from "../patreon-donor-pfps/CHG Jake.webp";
 import Colyn from "../patreon-donor-pfps/Colyn.webp";
@@ -16,7 +17,9 @@ import HopeVictoriaLiepe from "../patreon-donor-pfps/Hope Victoria Liepe.webp";
 import ItsMe from "../patreon-donor-pfps/It's Me.webp";
 import JoshuaPerez from "../patreon-donor-pfps/Joshua Perez.webp";
 import Katsam87 from "../patreon-donor-pfps/Katsam87.webp";
+import Nagapup from "../patreon-donor-pfps/Nagapup.webp";
 import rae from "../patreon-donor-pfps/rae.webp";
+import RichardTaggart from "../patreon-donor-pfps/Richard Taggart.webp";
 import TheBoltLord from "../patreon-donor-pfps/TheBoltLord ..webp";
 
 const PatreonDonors = () => {
@@ -32,6 +35,23 @@ const PatreonDonors = () => {
         </div>
       </div>
       <div className="row">
+        <div className="col-12 col-md-4 p-2 h-100">
+          <Paper className={`p-2 h-100 ${styles.patreon}`}>
+            <Image
+              className={styles.pfp}
+              width="100"
+              height="100"
+              src={Alybeanz.src}
+              alt="Alybeanz's Patreon Profile Picture"
+              blurDataURL={Alybeanz.blurDataURL}
+            />
+            <div className={styles.infoContainer}>
+              <span className={styles.name}>Alybeanz</span>
+              <span>Since {moment("2022-09-23").fromNow()}</span>
+              <span>$1</span>
+            </div>
+          </Paper>
+        </div>
         {/* <div className="col-12 col-md-4 p-2 h-100">
           <Paper className={`p-2 h-100 ${styles.patreon}`}>
             <Image
@@ -151,6 +171,23 @@ const PatreonDonors = () => {
             </div>
           </Paper>
         </div>
+        <div className="col-12 col-md-4 p-2 h-100">
+          <Paper className={`p-2 h-100 ${styles.patreon}`}>
+            <Image
+              className={styles.pfp}
+              width="100"
+              height="100"
+              src={Nagapup.src}
+              alt="Nagapup's Patreon Profile Picture"
+              blurDataURL={Nagapup.blurDataURL}
+            />
+            <div className={styles.infoContainer}>
+              <span className={styles.name}>Nagapup</span>
+              <span>Since {moment("2022-09-03").fromNow()}</span>
+              <span>£1</span>
+            </div>
+          </Paper>
+        </div>
         {/* <div className="col-12 col-md-4 p-2 h-100">
           <Paper className={`p-2 h-100 ${styles.patreon}`}>
             <Image
@@ -165,6 +202,23 @@ const PatreonDonors = () => {
               <span className={styles.name}>rae</span>
               <span>Since {moment("2022-08-14").fromNow()}</span>
               <span>$2</span>
+            </div>
+          </Paper>
+        </div> */}
+        {/* <div className="col-12 col-md-4 p-2 h-100">
+          <Paper className={`p-2 h-100 ${styles.patreon}`}>
+            <Image
+              className={styles.pfp}
+              width="100"
+              height="100"
+              src={RichardTaggart.src}
+              alt="Richard Taggart's Patreon Profile Picture"
+              blurDataURL={RichardTaggart.blurDataURL}
+            />
+            <div className={styles.infoContainer}>
+              <span className={styles.name}>Richard Taggart</span>
+              <span>Since {moment("2022-09-04").fromNow()}</span>
+              <span>$1</span>
             </div>
           </Paper>
         </div> */}
