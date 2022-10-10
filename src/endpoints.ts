fe0e24fdfd7d58396d9c848d05f8877e6b3281b2
ScribbleNerd <contact@scribblenerd.com>
2022-10-10 15:20:10 -0500

    Update endpoints

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,8 +1,19 @@
 import config from "./config.json";
+import fs from "fs";
+
+let branchId;
+if (fs.existsSync("branch.json")) {
+  const branch = JSON.parse(fs.readFileSync("branch.json", "utf-8"));
+  branchId = branch.branchId;
+}
 
 const isDev = process.env.NODE_ENV === "development";
 
-const endpoint = isDev ? "http://localhost:3000/api" : "https://unusann.us/api"; //process.env["localApiPort"] ? `http://localhost:${process.env["localApiPort"]}` : "https://api.unusann.us";
+const endpoint = isDev
+  ? "http://localhost:3000/api"
+  : branchId === "beta"
+  ? "https://beta.unusann.us/api"
+  : "https://unusann.us/api";
 const localApi = isDev ? "http://localhost:3000/api" : "https://unusann.us/api";
 const siteRoot = isDev ? "http://localhost:3000" : "https://unusann.us";
 const cdn = process.env["cdn"] ?? "https://usc1.contabostorage.com/a7f68355d8c442d8a7a1076a0ac5d924:videos";
