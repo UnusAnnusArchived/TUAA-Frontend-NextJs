e328fc6293a72c600f04dc09d9ecb1768b4c3ad4
ScribbleNerd <contact@scribblenerd.com>
2023-03-17 13:31:36 -0500

    Add maintenance date

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,8 @@ import { useRouter } from "next/router";
 import { initTranslations } from "../src/i18n/i18n";
 import NonSsrWrapper from "../components/non-ssr-wrapper";
 import ThemeProvider from "../components/theme";
+import { Paper, Typography } from "@mui/material";
+import moment from "moment-with-locales-es6";
 
 export const reportWebVitals = (metric: NextWebVitalsMetric) => {
   const { id, name, label, value } = metric;
@@ -46,6 +48,10 @@ const App = ({ Component, pageProps }: AppProps) => {
         <ThemeProvider>
           <GeistProvider>
             <NonSsrWrapper>
+              <Typography sx={{ textAlign: "center" }}>
+                The site is going to be offline due to planned maintenance on{" "}
+                {moment(1679504400000).format("MMMM Do YYYY [at] LT UTCZ")} (in {moment(1679504400000).fromNow()})
+              </Typography>
               <Component {...pageProps} />
             </NonSsrWrapper>
           </GeistProvider>
