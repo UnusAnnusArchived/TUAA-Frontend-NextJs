a57cfd01215ba01a01848937e89708304d6661de
ScribbleNerd <contact@scribblenerd.com>
2023-04-01 23:48:13 -0500

    Remove maintenance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,11 +48,6 @@ const App = ({ Component, pageProps }: AppProps) => {
         <ThemeProvider>
           <GeistProvider>
             <NonSsrWrapper>
-              <Typography sx={{ textAlign: "center" }}>
-                The site is going to be offline due to planned maintenance on{" "}
-                {moment(1679504400000).format("MMMM Do YYYY [at] LT UTCZ")} (in {moment(1679504400000).fromNow()}) and
-                on {moment(1679670000000).format("MMMM Do YYYY [at] LT UTCZ")} (in {moment(1679670000000).fromNow()}).
-              </Typography>
               <Component {...pageProps} />
             </NonSsrWrapper>
           </GeistProvider>
