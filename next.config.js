269694464bff4f4dc5a1b15c7dce81df905ff6bf
Everly Scott <everly@everly.page>
2025-01-01 03:54:13 -0600

    no turbo on prod+remove unneeded config for next

--- next.config.js
+++ next.config.js
@@ -10,17 +10,10 @@ const withPWA = require("next-pwa")({
 });
 
 module.exports = withPWA({
-  webpack: (config) => {
-    // config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
-    return config;
-  },
   reactStrictMode: true,
   images: {
     domains: ["r2.unusann.us", "rust-lore.dpaste.org"],
   },
   productionBrowserSourceMaps: true,
   output: "standalone",
-  experimental: {
-    turbo: {},
-  },
 });
